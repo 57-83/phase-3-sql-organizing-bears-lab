4d7be7bfd52f57e8a47ce01ee0510de6208c02fb
+ <+>
2023-02-17 21:25:26 +0300

    completed organizing bears

---
+++ lib/insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES("Mr. Chocolate", 5, "M", "WHITE","sijui",0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES("Rowdy", 5, "M", "WHITE","sijui",1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES("Tabitha", 1, "F", "WHITE","sijui",0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES("Sergeant Brown", 5, "M", "WHITE","sijui",1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES("Melissa", 4,
